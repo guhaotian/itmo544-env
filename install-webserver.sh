73d74319d6501160cd155aef518b910c25091dda
guhaotian <guhaotianwufan@gmail.com>
2015-10-03 13:09:47 -0500

    added code to pull index from other github

--- install-webserver.sh
+++ install-webserver.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 sudo apt-get update -y
-sudo apt-get install -y apache2
+sudo apt-get install -y apache2 git 
+git clone https://github.com/guhaotian/itmo544-2015.git
+mv ./itmo544-2015/images /var/www/html/images
+mv ./itmo544-2015/index.html var/www/html
 
 echo "hello"> /temp/hello.txt
 
