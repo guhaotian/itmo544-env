7e7332ee0527d24365e80a5af6c107720069bec3
guhaotian <guhaotianwufan@gmail.com>
2015-10-03 23:15:26 -0500

    added install-service.sh add move page2 to .sh

--- install-webserver.sh
+++ install-webserver.sh
@@ -5,6 +5,6 @@ sudo apt-get install -y apache2 git
 git clone https://github.com/guhaotian/itmo544-2015.git
 mv ./itmo544-2015/images /var/www/html/images
 mv ./itmo544-2015/index.html var/www/html
-
+mv ./itmo544-2015/page2.html var/www/html
 echo "hello"> /temp/hello.txt
 
